a6412b98c84ea487de9520b0cbfb1602af35f799
davifprado <35809055+davifprado@users.noreply.github.com>
2021-07-05 13:40:38 -0300

    1

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,4 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render( <h1> React </h1>, document.getElementById('root'))
+// import Primeiro from './componentes/Primeiro'
+// import BomDia from './componentes/BomDia'
+// import Multiplos, { BoaNoite } from './componentes/Multiplos'
+// import Multiplos from './componentes/Multiplos'
+
+// import Saudacao from './componentes/Saudacao'
+
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filhos'
+
+ReactDOM.render( 
+    <div>
+        
+        <Pai nome="Jana"  sobrenome="Ribeiro">
+            <Filho nome="Pedro" />
+            <Filho nome="Caio" />
+            <Filho nome="Paulo" />
+        </Pai>
+
+    </div>
+
+    , document.getElementById('root'))
