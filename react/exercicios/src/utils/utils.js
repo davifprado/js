75a1cd0fd74a6577e3b65ba748230e8220256b44
davifprado <35809055+davifprado@users.noreply.github.com>
2021-07-05 17:49:48 -0300

    1

--- react/exercicios/src/utils/utils.js
+++ react/exercicios/src/utils/utils.js
@@ -1,5 +1,5 @@
 import React from 'react'
-export default function childrenWithProps(props){
+export default function ChildrenWithProps(props){
     return React.Children.map(props.children, child => {
         return React.cloneElement(child, {
             ...props, ...child.props
